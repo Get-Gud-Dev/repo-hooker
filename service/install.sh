b9832eb9bc38c1acf9e41b1fa98c9efc92bfc678
Getten Gud <Gettengud@gmail.com>
2019-12-24 16:18:38 -0500

    Maybe have service setup correctly

--- service/install.sh
+++ service/install.sh
@@ -1,2 +1,5 @@
+sudo cp repo-hooker.sh /usr/bin/repo-hooker.sh
+sudo chmod +x /usr/bin/test_service.sh
+
 sudo cp repo-hooker.service /etc/systemd/system/repo-hooker.service
 sudo chmod 644 /etc/systemd/system/repo-hooker.service
