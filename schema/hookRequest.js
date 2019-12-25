5cd6d5231c1dbd488e7d5f17f8b12c4d121ff534
Getten Gud <Gettengud@gmail.com>
2019-12-24 22:10:42 -0500

    oh deer

--- schema/hookRequest.js
+++ schema/hookRequest.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose')
 
-const hookRequestSchema = 
+const hookRequestSchema = new mongoose.Schema(
 {
     Type: String,
     IP: String,
     URL: String,
     Time: Date,
     Log: String
-}
+})
 
 exports.Model = mongoose.Model("Hook Request", hookRequestSchema)
