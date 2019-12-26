ee7f9035d5ccd464004befd3bd10060cd20c2452
Getten Gud <Gettengud@gmail.com>
2019-12-26 13:11:10 -0500

    Oh dear

--- puller.js
+++ puller.js
@@ -7,35 +7,41 @@ var active_builds = {}
 
 exports.pullRepo = (endpoint) => {
 
+    try{
+        let build = new Build({output:"Build Initiated at " + Date.now().toString()})
     
+        if(active_builds[endpoint.label] != null)
+        {
+            build.output += "A build for this path is already in progress, I'm cancelling and letting it finish"
+            return;
+        }
     
-    let build = new Build({output:"Build Initiated at " + Date.now().toString()})
+        active_builds[endpoint.label] = build
     
-    if(active_builds[endpoint.label] != null)
-    {
-        build.output += "A build for this path is already in progress, I'm cancelling and letting it finish"
-        return;
-    }
-
-    active_builds[endpoint.label] = build
-
-    const pullProc = spawn("git pull", {cwd:endpoint.path})
-    
-    pullProc.stdout.on('data', (data) =>{
-        build.output += "\n" + data;
-    })
-
-    pullProc.stderr.on('data', (data) =>{
-        build.output += "\n" + data;
-    })
-
-    pullProc.on('exit', function(){
-        build.output += "\nComplete!"
-        build.save((err) => {
-            delete(active_builds[endpoint.label])
+        const pullProc = spawn("git pull", {cwd:endpoint.path})
+        
+        pullProc.stdout.on('data', (data) =>{
+            build.output += "\n" + data;
         })
-    })
-    return pullProc
+    
+        pullProc.stderr.on('data', (data) =>{
+            build.output += "\n" + data;
+        })
+    
+        pullProc.on('exit', function(){
+            build.output += "\nComplete!"
+            build.save((err) => {
+                delete(active_builds[endpoint.label])
+            })
+        })
+        return pullProc
+    }
+    catch(e){
+        console.log(e)
+    }
+    
+    
+    
 }
 
 
