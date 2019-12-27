6057aeb2368a01f9efabecb09bd7d9e567acaa99
Getten Gud <Gettengud@gmail.com>
2019-12-26 23:12:46 -0500

    Dumb george michael

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.post('/update/github/:repo', jsonEncodedParser, function (req,res) {
     project.findOne( {label: req.params.repo.toLowerCase()}, (err, doc) => {
         if(doc != null)
         {
-            if(req.get['x-hub-signature'] != null){
+            if(req.get('X-Hub-Signature') != null){
                 let remoteSecret = req.get['X-Hub-Signature'].split('=')[1]
                 let computedSecret = crypto.createHmac('sha1', doc.secret).update(JSON.stringify(req.body)).digest('hex')
                 if(crypto.timingSafeEqual(Buffer.from(computedSecret, 'utf8'), Buffer.from(remoteSecret, 'utf8')))
